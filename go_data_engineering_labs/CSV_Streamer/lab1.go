40c9b9ce89cfd14d3aa000fa921aba9c62cafc71
Mohammad Bayat <m21newyork@gmail.com>
2026-04-26 01:08:02 -0400

    Started implemeting lab1

--- go_data_engineering_labs/CSV_Streamer/lab1.go
+++ go_data_engineering_labs/CSV_Streamer/lab1.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"github.com/brianvoe/gofakeit/v7";
+	"fmt"
+)
+type customers struct {
+	first_name string
+	last_name string 
+	purchased bool
+	items_bought []string
+	item_category string
+}
+func lab1() {
+	for i:=0;i<10;i++ {
+		first_name := gofakeit.FirstName()
+		fmt.Println(first_name)
+
+	}
+
+}
+
