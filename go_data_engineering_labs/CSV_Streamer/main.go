52682ecf6637ab2b7fc5bf415739494968f4ab13
Mohammad Bayat <m21newyork@gmail.com>
2026-04-27 14:13:07 -0400

    working code to upload to azure blob

--- go_data_engineering_labs/CSV_Streamer/main.go
+++ go_data_engineering_labs/CSV_Streamer/main.go
@@ -1,11 +1,16 @@
 package main
-import(
-	   "context"
-	   "flag"
-	   "fmt"
-	   "io"
-	   "os"
-	   "github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/bloberror"
+	"github.com/joho/godotenv"
 )
 
 
@@ -18,8 +23,13 @@ func main() {
 	numPtr := flag.Int("count",10,"number of customers to generate")
 	containerPtr := flag.String("container", "lab-data", "target Azure Blob container")
 	blobPtr := flag.String("blob", "generated_customers.csv", "target blob name")
+	createContainerPtr := flag.Bool("create-container", true, "create the target container if it does not exist")
 	flag.Parse()
-	
+
+	// Load local secrets file if present (does not override existing env vars).
+	if err := godotenv.Load(".env.local"); err != nil && !errors.Is(err, os.ErrNotExist) {
+		fmt.Printf("note: could not load .env.local: %v\n", err)
+	}
 
 	connStr := os.Getenv("AZURE_STORAGE_CONNECTION_STRING")
 	if connStr == "" {
@@ -33,9 +43,17 @@ func main() {
 		return
 	}
 
+	ctx := context.Background()
+	if *createContainerPtr {
+		_, err = client.CreateContainer(ctx, *containerPtr, nil)
+		if err != nil && !bloberror.HasCode(err, bloberror.ContainerAlreadyExists) {
+			fmt.Printf("could not create container %q: %v\n", *containerPtr, err)
+			return
+		}
+	}
+
 	go lab1(*numPtr, pw)
 
-	ctx := context.Background()
 	_, err = client.UploadStream(ctx, *containerPtr, *blobPtr, pr, nil)
 	if err != nil {
 		fmt.Printf("could not upload CSV stream to blob: %v\n", err)
